4b4cee7ca61b41946a7b092199b5169154c5d7e8
FTCPhantom16950 <79818770327@yandex.com>
2025-10-16 19:23:48 +0300

    starting commit

---
+++ PhantomLIb/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.library)
+    id("maven-publish")
+}
+
+android {
+    publishing {
+        multipleVariants {
+            allVariants()
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+    namespace = "io.github.ftcphantom16950.phantomlib"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation ("org.firstinspires.ftc:Inspection:11.0.0")
+    implementation ("org.firstinspires.ftc:Blocks:11.0.0")
+    //noinspection Aligned16KB
+    implementation ("org.firstinspires.ftc:RobotCore:11.0.0")
+    implementation ("org.firstinspires.ftc:RobotServer:11.0.0")
+    implementation ("org.firstinspires.ftc:OnBotJava:11.0.0")
+    implementation ("org.firstinspires.ftc:Hardware:11.0.0")
+    implementation ("org.firstinspires.ftc:FtcCommon:11.0.0")
+    implementation ("org.firstinspires.ftc:Vision:11.0.0")
+}
+afterEvaluate {
+    publishing{
+        publications{
+            create<MavenPublication>("mavenRelease"){
+                groupId = "io.github.ftcphantom16950"
+                artifactId = "PhantomLib"
+                version = "0.1"
+
+                from(components["release"])
+            }
+        }
+    }
+}
